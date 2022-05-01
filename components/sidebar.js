8f7578468710da34f0e808704895b50fe9057421
David <DavidMelnychuk@users.noreply.github.com>
2022-04-30 23:47:52 -0700

    add redirect middleware

--- components/sidebar.js
+++ components/sidebar.js
@@ -5,12 +5,20 @@ import {
   PlusCircleIcon,
   HeartIcon,
   RssIcon,
-} from '@heroicons/react/outline'
+} from '@heroicons/react/outline';
+import { signOut, useSession } from 'next-auth/react';
 
 function Sidebar() {
+  const { data: session, status } = useSession();
   return (
     <div className="border-r border-gray-900 p-5 text-sm text-gray-500">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Log out</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
@@ -50,7 +58,7 @@ function Sidebar() {
         <p className="cursor-pointer hover:text-white">PlayList name...</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
